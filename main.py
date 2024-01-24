2de90424352af239cdc66b838d796f0f88797357
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2024-01-24 20:49:58 +0100

    Refactor. Final cosmetic changes

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ import random
 fake = Faker()
 today = date.today()
 
+movies_list = []
+genres_list = ["Action", "Drama", "Comedy", "Sci-Fi", "Document", "Historical"]
+
+def main(movies_list):
+    print("Biblioteka filmów.\n")
+    movies_list += create_movies_list('serial', 4096)
+    movies_list += create_movies_list('movie', 4096)
+    movies_list += create_serial_full_season('Zorro', 2024, 'Action', 2, 13)
+    movies_list += create_serial_full_season('Star Trek', 2000, 'Sci-Fi', 1, 24)
+    generate_views_x_times(8192)
+    date_formatted = today.strftime("%d.%m.%Y")
+    print(f"Najpopularniejsze filmy i seriale dnia {date_formatted}\n")
+    show(top_titles(3))
+
 def get_selected_type(type):
     if type == 'movies' or type == 'series':
         series = []
@@ -24,11 +38,11 @@ def get_selected_type(type):
         return False
         
 def search(title):
-    search_results = []
+    results = []
     for element in movies_list:
         if title.strip().lower() == (element.title).strip().lower():
-            search_results.append(element)
-    return search_results
+            results.append(element)
+    return results
 
 def generate_views():
     min = 0
@@ -44,13 +58,8 @@ def generate_views_x_times(x=10):
         generate_views()
 
 def top_titles(how_many=10, content_type=False):
-    if not content_type:
-        return sorted(movies_list, key=lambda element: element.play_counter)[-how_many:]
-    else:
-        if content_type == 'series':
-            return sorted(get_series(), key=lambda element: element.play_counter)[-how_many:]
-        elif content_type == 'movies':
-            return sorted(get_movies(), key=lambda element: element.play_counter)[-how_many:]
+    selected = get_selected_type(content_type) if content_type else movies_list
+    return sorted(selected, key=lambda element: element.play_counter)[-how_many:][::-1]
         
 def create_serial_episode(episode, season, index_in_array, title, year, genre):
     return Serial(episode,
@@ -61,7 +70,7 @@ def create_serial_episode(episode, season, index_in_array, title, year, genre):
                   genre,
                   )        
 
-def add_serial_full_season(title, year, genre, season, episodes):
+def create_serial_full_season(title, year, genre, season, episodes):
     array = []
     for episode_index in range(1, episodes):
         index_in_array = len(movies_list)
@@ -76,35 +85,35 @@ def count_serial_episodes(title):
     return episodes_counter
     
 def get_random_genre():
-    return random.choice(["Action", "Drama", "Comedy", "Sci-Fi", "Document", "Historical"])
+    return random.choice(genres_list)
 
 def get_random_year():
     return random.randint(1950, 2023)
 
-def create_fake_movie(index_in_array):
-    return Movie(index_in_array,
-                  fake.name(),
-                  get_random_year(),
-                  get_random_genre(),
-                  )
-
-def create_fake_serial(index_in_array):
-    return Serial(random.randint(1, 20),
-                  random.randint(1, 10),
-                  index_in_array,
-                  fake.name(),
-                  get_random_year(),
-                  get_random_genre(),
-                  )
+def create_fake_item(type, index_in_array):
+    if type == 'movie':
+        return Movie(index_in_array,
+                    fake.name(),
+                    get_random_year(),
+                    get_random_genre(),
+                    )
+    elif type == 'serial':
+        return Serial(random.randint(1, 20),
+                    random.randint(1, 10),
+                    index_in_array,
+                    fake.name(),
+                    get_random_year(),
+                    get_random_genre(),
+                    )
+    else:
+        return False
     
-def create_list(type='movie', how_many=1):
+def create_movies_list(type='movie', how_many=10):
     if type == 'movie' or type == 'serial':
         array = []
         for index in range(0, how_many):
-            array.append(
-                create_fake_movie(index) if type == 'movie' 
-                else create_fake_serial(index)
-                )    
+            new_item = create_fake_item(type, index)
+            array.append(new_item)    
         return array
     else:
         print("Error. Wrong type. Only movie or serial.")
@@ -117,15 +126,4 @@ def show(list):
     for element in list:
         print(repr(element))
     
-    
-print("Biblioteka filmów.\n")
-movies_list = []
-movies_list += create_list('serial', 2048)
-movies_list += create_list('movie', 2048)
-movies_list += add_serial_full_season('Zorro', 2024, 'Action', 2, 13)
-movies_list += add_serial_full_season('Star Trek', 2000, 'Sci-Fi', 1, 24)
-generate_views_x_times(4096)
-
-date_formatted = today.strftime("%d.%m.%Y")
-print(f"Najpopularniejsze filmy i seriale dnia {date_formatted}\n")
-show(top_titles(3))
+main(movies_list)
